402685c0e901cbb54188a2d3ad56ddc03b1f0c39
De Li <de_li@live.com>
2016-11-16 13:40:09 -0800

    multiselection support, automatically add new line, add braces

--- AutoSemiColon.py
+++ AutoSemiColon.py
@@ -1,32 +1,38 @@
+# format
 import sublime_plugin
 import sublime
 
 
 class AutoSemiColonCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        # Loop through and add the semi colon
-        for sel in self.view.sel():
-            # The last letter we've dealt with
-            first = sel.end()
-            self.view.insert(edit, first, ';')
 
-        # Loop through and add move it to the end
-        for sel in self.view.sel():
+    def run(self, edit):
+        view = self.view
+        last_pos = []
+        smart_insert = True
+        for sel in view.sel():
+            if not sel.empty():
+                smart_insert = False
+                break
+
             last = last_bracket = first = sel.end()
-            # Find the last bracket
-            while (self.view.substr(last) in [' ', ')', ']']):
-                if (self.view.substr(last) != ' '):
+            while (view.substr(last) in [' ', ')', ']', '}']):
+                if (view.substr(last) != ' '):
                     last_bracket = last + 1
                 last += 1
 
             if (last_bracket < last):
                 last = last_bracket
 
-            # Can we insert the semi colon elsewhere?
             if last > first:
-                self.view.erase(edit, sublime.Region(first - 1, first))
-                # Delete the old semi colon
-                self.view.insert(edit, last - 1, ';')
-                # Move the cursor
-                self.view.sel().clear()
-                self.view.sel().add(sublime.Region(last, last))
+                last_pos.append(last)
+            else:
+                smart_insert = False
+                break
+
+        if smart_insert:
+            view.sel().clear()
+            for last in last_pos:
+                view.sel().add(sublime.Region(last, last))
+            view.run_command("insert", {"characters": ";\n"})
+        else:
+            view.run_command("insert", {"characters": ";"})
